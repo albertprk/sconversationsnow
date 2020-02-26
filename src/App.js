63735ebd2bbd11eac0f3224cdf8c4e05dc47a8f0
Ben C <mrbenc88@students.cs.ubc.ca>
2020-02-25 20:33:10 -0800

    Added login page base with welcome back panel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import Landing from './components/Landing'
+import Login from './components/Login'
+import './components/Login.css';
 
 class App extends Component {
   render() {
     return (
-        <Landing/>
+        //<Landing/>
+        <Login/>
     )
   }
 
---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Login = props => (
+    <div className="logIn">
+        
+        <div className="rectangle">
+            <div className = "welcomeBack">Welcome<br></br>Back</div>
+        </div>
+    </div>
+);
+
+export default Login;
