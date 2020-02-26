c884df010518d14f08852fd0a81dcc9cf13889e0
Albert Park <albert.park@hotmail.com>
2020-02-25 19:48:08 -0800

    Landing page button and infocards completed

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,10 @@ const Landing = props => (
     <div className="landingbg">
         <div className="landingtext">Connect to<br/> Your campus</div>
         <div className="info">
+            <p className="infotext">It's that simple</p>
             <InfoCard/> <InfoCard/>
+            <br/>
+            <button className="chatbutton">Let's Chat</button>
         </div>
     </div>
 );
