7972565da58767832aaf06fe59b22769bfd70436
Ben C <mrbenc88@students.cs.ubc.ca>
2020-02-25 20:49:00 -0800

    Added remaining login prompts

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,8 +9,17 @@ const Login = props => (
             <div className = "welcomeBack">Welcome<br></br>Back</div>
         </div>
         <div className = "logInWithGoogle">Log in with Google</div>
+
         <div className = "rectangle_2">     
         </div>
+        <div className = "logInWithFacebook">Log in with Facebook</div>
+        <div className = "rectangle_2_1 ">     
+        </div>
+        <div className = "logInWithEmail">Log in with Email</div>
+        <div className = "rectangle_2_2 ">     
+        </div>
+
+
         
 
     </div>
