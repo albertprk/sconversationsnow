d91ec6c20730e933a9c38a220013b6c1c7cfbc56
Albert Park <albert.park@hotmail.com>
2020-02-25 20:05:03 -0800

    Added images to infocards

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,8 @@ const Landing = props => (
         <div className="landingtext">Connect to<br/> Your campus</div>
         <div className="info">
             <p className="infotext">It's that simple</p>
-            <InfoCard/> <InfoCard/>
+            <InfoCard image={"../images/choosegroup.png"}/> <InfoCard image="../images/talkstudents.png"/>
+            <img src={"./choosegroup.png"}></img>
             <br/>
             <button className="chatbutton">Let's Chat</button>
         </div>
