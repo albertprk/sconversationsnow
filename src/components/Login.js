7675f54af1ea83197ac1ba3eedd0a992ae555688
alopes02 <anthony.lopes@mail.utoronto.ca>
2020-03-05 19:35:52 -0800

    progress

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,21 +8,21 @@ const Login = props => (
             <div className = "welcomeBack">Welcome<br></br>Back</div>
         </div>
         <div className="rectangleRight">
-        <div className = "newGoogleFavicon_512"/>
-        <div className = "logInWithGoogle">Log in with Google</div>
-        <div className = "rectangle_2"/>  
-        <div className = "facebookCircle_512"/>           
-        <div className = "logInWithFacebook">Log in with Facebook</div>
-        <div className = "or"> or </div>
+            <div className = "newGoogleFavicon_512"/>
+            <div className = "logInWithGoogle">Log in with Google</div>
+            <div className = "rectangle_2"/>  
+            <div className = "facebookCircle_512"/>           
+            <div className = "logInWithFacebook">Log in with Facebook</div>
+            <div className = "or"> or </div>
         
-        <div className = "rectangle_2_1 ">     
-        </div>
-        <div className = "logInWithEmail">Log in with Email</div>
-        <div className = "rectangle_2_2 "/>    
+            <div className = "rectangle_2_1 ">     
+            </div>
+            <div className = "logInWithEmail">Log in with Email</div>
+            <div className = "rectangle_2_2 "/>    
 
-        <div className="dontHaveAnAccount">
-            Don't have an account? <span className="sign-up-button">Sign Up</span>
-        </div> 
+            <div className="dontHaveAnAccount">
+                Don't have an account? <span className="sign-up-button">Sign Up</span>
+            </div> 
         </div>
         
         
